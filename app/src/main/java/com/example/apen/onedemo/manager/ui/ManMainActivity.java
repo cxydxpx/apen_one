1078a2eccebbb81a14bdf1f1773119f955c4ecc8
cxydxpx <578479132@qq.com>
2017-05-10 17:15:19 +0800

    git 我又来了

--- app/src/main/java/com/example/apen/onedemo/manager/ui/ManMainActivity.java
+++ app/src/main/java/com/example/apen/onedemo/manager/ui/ManMainActivity.java
@@ -18,6 +18,15 @@ public class ManMainActivity extends BaseActivity {
     @BindView(R.id.tv_place_order)
     TextView mPlaceOrder;
 
+    @BindView(R.id.tv_order)
+    TextView mOrder;
+
+    @BindView(R.id.tv_report)
+    TextView mReport;
+
+    @BindView(R.id.tv_sales)
+    TextView mSales;
+
     @Override
     protected int layoutResId() {
         return R.layout.activity_man_main;
@@ -40,14 +49,34 @@ public class ManMainActivity extends BaseActivity {
     @Override
     protected void initListener() {
         mPlaceOrder.setOnClickListener(this);
+        mOrder.setOnClickListener(this);
+        mReport.setOnClickListener(this);
     }
 
     @Override
     protected void processClick(View v) {
         switch (v.getId()) {
             case R.id.tv_place_order:
-                Intent intent = new Intent(this,ManGoodsActivity.class);
+                Intent intent = new Intent(this, ManGoodsActivity.class);
                 startActivity(intent);
+
+                break;
+            case R.id.tv_order:
+
+                Intent intent1 = new Intent(this,OrderQueryActivity.class);
+                startActivity(intent1);
+
+                break;
+            case R.id.tv_report:
+
+                Intent intent2 = new Intent(this,ReportActivity.class);
+                startActivity(intent2);
+
+                break;
+            case R.id.tv_sales:
+
+                Intent intent3 = new Intent(this,SalesActivity.class);
+                startActivity(intent3);
 
                 break;
             default:
---
+++ app/src/main/java/com/example/apen/onedemo/manager/ui/ReportActivity.java
@@ -0,0 +1,42 @@
+package com.example.apen.onedemo.manager.ui;
+
+import android.view.View;
+
+import com.example.apen.onedemo.BaseActivity;
+import com.example.apen.onedemo.R;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ */
+
+public class ReportActivity extends BaseActivity{
+    @Override
+    protected int layoutResId() {
+        return R.layout.activity_report;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void processClick(View v) {
+
+    }
+}
